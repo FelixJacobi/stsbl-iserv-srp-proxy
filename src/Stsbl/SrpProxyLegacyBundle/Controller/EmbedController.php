2d24cc2a28e5f8ff6109a0b38ffcca9fa6fc2bd5
Felix Jacobi <felix.jacobi@stsbl.de>
2017-01-28 20:06:15 +0100

    Updated copyright headers.

--- src/Stsbl/SrpProxyLegacyBundle/Controller/EmbedController.php
+++ src/Stsbl/SrpProxyLegacyBundle/Controller/EmbedController.php
@@ -2,17 +2,42 @@
 // src/Stsbl/SrpProxyLegacyBundle/Controller/EmbedController.php
 namespace Stsbl\SrpProxyLegacyBundle\Controller;
 
-use IServ\CoreBundle\Controller\EmbedController as ParentEmbedController;
+use IServ\CoreBundle\Controller\EmbedController as BaseEmbedController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Stsbl\SrpProxyLegacyBundle\Menu\SrpProxyLegacyModuleLoader;
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Felix Jacobi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 /**
  * Controller which loading legacy modules in the new IDesk.
  *
  * @author Felix Jacobi <felix.jacobi@stsbl.de>
- * @license http://gnu.org/licenses/gpl-3.0 GNU General Public license
+ * @license MIT license <https:/opensoruce.org/licenses/MIT>
  */
-class EmbedController extends ParentEmbedController {
+class EmbedController extends BaseEmbedController 
+{
     /**
      * Controls the embedding of legacy pages
      *
